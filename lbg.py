34d7508eaf2b251cb5d47ec4113b7a5fa9198292
stevenpwilson <stevenwilson-289@hotmail.co.uk>
2022-11-18 11:29:52 +0000

    updated python

--- lbg.py
+++ lbg.py
@@ -2,6 +2,9 @@
 LBG learning-oriented CRUD-based RESTful API using standard Flask routing
 """
 
+#Update to program to then upload to github
+
+
 # import Flask microframework and associated tools
 from flask import Flask, request, jsonify
 from flask_api import status
@@ -37,7 +40,7 @@ def item_builder(item_name: str, item_description: str, item_price: float, item_
     uses standard library dictionary to create an item object
     """
     item: dict[str | int | float] = {
-        "name": item_name,
+        "name of item": item_name,
         "description": item_description,
         "price": item_price,
         "_id": item_id,
